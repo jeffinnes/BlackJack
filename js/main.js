e49f7c45196baed9c5d0beb1c2b020d513add671
jeffinnes <jeffdango@gmail.com>
2017-09-20 22:09:10 -0700

    Begin defining control areas

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/**
+ * UI controller
+ *
+ * This section controls the visual aspects of the app.
+ * (Initializing play area, adding cards to the hands, or updating values)
+ */
+
+
+
+
+
+/**
+ * Game controller
+ *
+ * This section controls the functionality of the game
+ * (building deck, checking win/lose conditions,
+ * responding to user input... probably... User input might go in UI controller.)
+ */
+
+
+
+
+
+/**
+ * Global controller
+ *
+ *
+ */
